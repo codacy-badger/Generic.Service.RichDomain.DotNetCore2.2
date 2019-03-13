ccb77b89e88013a296ccf4386f86662c191751b2
Guilherme Lúcio <guilherme.lpcaixeta@gmail.com>
2019-03-13 11:11:30 -0300

    Fix source

--- src/Generic.Repository/Extensions/Properties/Properties.cs
+++ src/Generic.Repository/Extensions/Properties/Properties.cs
@@ -8,13 +8,13 @@ namespace Generic.Repository.Extensions.Properties
     public static class Properties<E>
     where E : class
     {
-        public static Dictionary<string, Dictionary<string, Func<E, object>>> CacheGet { get; private set; } = new Dictionary<string, Dictionary<string, Func<E, object>>>();
-        public static Dictionary<string, Dictionary<string, Action<E, object>>> CacheSet { get; private set; }= new Dictionary<string, Dictionary<string, Action<E, object>>>();
+        public static Dictionary<string, Dictionary<string, Func<E, object>>> CacheGet { get; private set; }
+        public static Dictionary<string, Dictionary<string, Action<E, object>>> CacheSet { get; private set; }
 
         public static void SetDefaultSizeOnCache()
         {
-            CacheGet = new Dictionary<string, Dictionary<string, Func<E, object>>>(Commom.Commom.totalTypesInAssemblyModel);
-            CacheSet = new Dictionary<string, Dictionary<string, Action<E, object>>>(Commom.Commom.totalTypesInAssemblyModel);
+            CacheGet = CacheGet ?? new Dictionary<string, Dictionary<string, Func<E, object>>>(Commom.Commom.totalTypesInAssemblyModel);
+            CacheSet = CacheSet ?? new Dictionary<string, Dictionary<string, Action<E, object>>>(Commom.Commom.totalTypesInAssemblyModel);
         }
     }
 }
